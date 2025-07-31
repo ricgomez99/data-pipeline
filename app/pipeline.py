4fe639377d66f0a4f9f43cb8f59c076a6b4ff4af
Andres <argomez.restrepo@gmail.com>
2025-07-30 19:29:36 -0500

    Add data process class

--- app/pipeline.py
+++ app/pipeline.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from simple-pipeline!")
+import os
+from helpers import DataProcess
+
+INPUT_PATH = os.path.join('/data', 'Medicaldataset.csv')
+OUTPUT_PATH = os.path.join('/data', 'CleanedMedicalData.csv')
+
+def run_pipeline():
+    process = DataProcess()
+    raw_data = process.extract_data(INPUT_PATH)
+    cleaned_data = process.transform_data(raw_data)
+    process.load_data(cleaned_data, OUTPUT_PATH)
+
+    print("Data pipeline process complete!")
 
 
 if __name__ == "__main__":
-    main()
+    run_pipeline()
